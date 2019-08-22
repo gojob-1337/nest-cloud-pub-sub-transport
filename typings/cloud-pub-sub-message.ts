3367833876cd9172c1e1f3538fecd93da1852dd3
VinceOPS <vince.g.135@gmail.com>
2019-08-22 14:43:24 +0200

    chore: update pkgs (and nack delay deprecation)

--- typings/cloud-pub-sub-message.ts
+++ typings/cloud-pub-sub-message.ts
@@ -6,36 +6,41 @@
 export type CloudPubSubMessage = {
   /** ID used to acknowledge the message receival. */
   ackId: string;
+
   /** Optional attributes for this message. */
   attributes: Record<string, string>;
+
   /** Contents of the message. */
   // data: Uint8Array;
   data: Buffer;
+
   /** ID of the message. */
   id: string;
+
   /** The length of the message data. */
   length: number;
+
   /** The time at which the message was published. */
   publishTime: string;
+
   /** The time at which the message was recieved by the subscription. */
   received: number;
+
   /** Acknowledge the message. */
   ack: () => void;
+
   /**
-   * Removes the message from our inventory and schedules it to be redelivered.
-   * If the `delay` parameter is unset, it will be redelivered immediately.
-   *
-   * Do not acknowledge the message (allows more messages to be
-   * retrieved if your limit was hit).
-   *
-   * @param delay The desired time to wait before the redelivery occurs.
+   * Removes the message from our inventory and schedules it to be
+   * redelivered immediately. Do **not** acknowledge the message
+   * (allows more messages to be retrieved if your limit was hit).
    */
-  nack: (delay?: number) => void;
+  nack: () => void;
 };
 
 export type CloudPubSubMessageData = {
   /** "pattern" to be matched for the current message  */
   pattern: string;
+
   /** (optional) payload embedding custom data for the current message */
   data?: Record<string, unknown>;
 };
